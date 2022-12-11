a29511bd1a188e0ab773be5e42c17bd669be7964
ThanosTsiamis <thatsiamis@gmail.com>
2022-12-11 16:43:36 +0100

    minor fix on the generalise function

--- testing.py
+++ testing.py
@@ -14,17 +14,20 @@ def process_data(dataframe):
     return dataframe
 
 
-def generalise_string(string):
-    for character in string:
-        if character.isupper():
-            string = string.replace(character, "U")
-        if character.islower():
-            string = string.replace(character, "l")
-        if character.isdigit():
-            string = string.replace(character, "d")
-        if character.isspace():
-            string = string.replace(character, "s")
-    return string
+def generalise_string(string, specificity_level=0):
+    if specificity_level >= 0:
+        original_part = list(string[:specificity_level])
+        edited_part = list(string[specificity_level:])
+    for index in range(len(string)):
+        if string[index].isupper():
+            edited_part[index] = "U"
+        elif string[index].islower():
+            edited_part[index] = "l"
+        elif string[index].isdigit():
+            edited_part[index] = "d"
+        elif string[index].isspace():
+            edited_part[index] = "s"
+    return "".join(original_part+edited_part)
 
 
 def find_unique_elements(generalised_string):
@@ -38,6 +41,8 @@ def feature_to_split_on(specificity_level, df):
         length = np.vectorize(len)
         return length(df)[:, 0]
     else:
+        gen = np.vectorize(generalise_string, excluded=['specificity_level'])
+        asdsad = gen(df[:, 0], 2)
         print("Split based on specificity_level of the first element")
 
 
@@ -47,7 +52,7 @@ def tree_grow(column, nmin=6):
     while node_list:
         current_node = node_list.pop(0)
         child_list = []
-        if len(current_node.data) < nmin:
+        if len(current_node.data) < nmin and current_node.specificity_level > 0:
             continue
         children_identifiers = feature_to_split_on(specificity_level=current_node.specificity_level,
                                                    df=current_node.data)
@@ -69,5 +74,5 @@ def tree_grow(column, nmin=6):
 if __name__ == "__main__":
     dataframe = read_data("testing.csv")
     dataframe = process_data(dataframe)
-    tree=tree_grow(dataframe)
+    tree = tree_grow(dataframe)
     print('123')
