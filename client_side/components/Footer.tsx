ba250a18caeafcc7d36138aa340b8b352fd0645e
ThanosTsiamis <thatsiamis@gmail.com>
2024-04-06 17:09:01 +0300

    Minor tailwind improvements in header and footer

--- client_side/components/Footer.tsx
+++ client_side/components/Footer.tsx
@@ -15,33 +15,38 @@ function Footer() {
                         </p>
                     </div>
 
-                    <div className="mb-6 md:mb-0">
+                    <div className="grid gap-0.5 lg:grid-cols-2 mb-2 md:mb-0">
                         <h5 className="mb-2 font-medium uppercase">Links</h5>
-
-                        <ul className="mb-0 list-none">
-                            <li>
-                                <a href="" className="text-neutral-800 dark:text-neutral-200">
-                                    Github Repository
-                                </a>
-                            </li>
-                            <li>
-                                <a href="" className="text-neutral-800 dark:text-neutral-200">
-                                    Master Thesis Paper (coming soon)
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://github.com/ThanosTsiamis"
-                                   className="text-neutral-800 dark:text-neutral-200">
-                                    Thanos Tsiamis&apos;s Github
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://github.com/qahtanaa"
-                                   className="text-neutral-800 dark:text-neutral-200">
-                                    Dr. A.A.A. (Hakim) Qahtan&apos;s Github
-                                </a>
-                            </li>
-                        </ul>
+                        <div className="mb-6 md:mb-0">
+                            <ul className="mb-0 list-none">
+                                <li>
+                                    <a href="" className="text-neutral-800 dark:text-neutral-200">
+                                        Github Repository
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="" className="text-neutral-800 dark:text-neutral-200">
+                                        Master Thesis Paper (coming soon)
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul className="mb-0 list-none">
+                                <li>
+                                    <a href="https://github.com/ThanosTsiamis"
+                                       className="text-neutral-800 dark:text-neutral-200">
+                                        Thanos Tsiamis&apos;s Github
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://github.com/qahtanaa"
+                                       className="text-neutral-800 dark:text-neutral-200">
+                                        Dr. A.A.A. (Hakim) Qahtan&apos;s Github
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -55,4 +60,5 @@ function Footer() {
         </footer>
     );
 }
+
 export default Footer;
